a3c2d87c3b4f8adff1fbf0d9dd8bda32a7c67b52
Vincent <vincentpham13657@gmail.com>
2020-04-12 22:41:28 -0700

    Posts

--- client/src/app/Shared/Components/Posts/posts.component.ts
+++ client/src/app/Shared/Components/Posts/posts.component.ts
@@ -16,18 +16,19 @@ import { FormBuilder, Validators, FormGroup } from "@angular/forms";
   styleUrls: ["./posts.component.scss"],
 })
 export class PostsComponent implements OnInit, OnDestroy {
-  @ViewChild("createTextPost", { read: ElementRef, static: false })
-  createTextPost: ElementRef;
-  @ViewChild("createImagePost", { read: ElementRef, static: false })
-  createImagePost: ElementRef;
-  @ViewChild("createLinkPost", { read: ElementRef, static: false })
-  createLinkPost: ElementRef;
+  @ViewChild("createTextButton", { read: ElementRef, static: false })
+  createTextButton: ElementRef;
+  @ViewChild("createImageButton", { read: ElementRef, static: false })
+  createImageButton: ElementRef;
+  @ViewChild("createLinkButton", { read: ElementRef, static: false })
+  createLinkButton: ElementRef;
   @ViewChild("createPostContent", { read: ElementRef, static: false })
   createPostContent: ElementRef;
   documentClickSubscription: Subscription;
 
   PostType = PostType;
   createPostType: PostType;
+  createPostForms: { [key: number]: FormGroup };
   createPostForm: FormGroup;
 
   constructor(
@@ -44,30 +45,50 @@ export class PostsComponent implements OnInit, OnDestroy {
         console.log(err);
       }
     );
-    this.createPostForm = this.formBuilder.group({
-      username: ["", Validators.required],
-      password: ["", Validators.required],
-    });
+
+    this.initFormGroup();
   }
 
   ngOnDestroy(): void {
     this.documentClickSubscription.unsubscribe();
   }
 
+  initFormGroup(): void {
+    const urlPattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+    this.createPostForms = {};
+    this.createPostForms[PostType.TEXT] = this.formBuilder.group({
+      title: ["", Validators.required],
+      description: [""],
+    });
+    this.createPostForms[PostType.IMAGE] = this.formBuilder.group({
+      title: ["", Validators.required],
+      image: [""],
+    });
+    this.createPostForms[PostType.LINK] = this.formBuilder.group({
+      title: ["", Validators.required],
+      link: ["", Validators.required, Validators.pattern(urlPattern)],
+    });
+
+    this.createPostForm = this.createPostForms[PostType.TEXT];
+  }
+
   onClickCreatePost(postType: PostType): void {
     this.createPostType = postType;
+    this.createPostForm = this.createPostForms[postType];
   }
 
   onCloseCreatePost(): void {
     this.createPostType = null;
+    this.initFormGroup();
   }
 
   onDocumentClick(target: any): void {
     if (
       !this.createPostContent.nativeElement.contains(target) &&
-      !this.createTextPost.nativeElement.contains(target) &&
-      !this.createImagePost.nativeElement.contains(target) &&
-      !this.createLinkPost.nativeElement.contains(target)
+      !this.createTextButton.nativeElement.contains(target) &&
+      !this.createImageButton.nativeElement.contains(target) &&
+      !this.createLinkButton.nativeElement.contains(target)
     ) {
       this.onCloseCreatePost();
     }
